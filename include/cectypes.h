3efa3449829d0d6c16ffd3a8d3151950374d11df
wolfgar <stephan.rafin@laposte.net>
2014-03-13 16:03:58 +0100

    Add iMX6 support Requires specific kernel driver. Check http://stephan-rafin.net/blog/2013/09/30/i-mx6-cec/
    Author:    wolfgar <stephan.rafin@laposte.net>

--- include/cectypes.h
+++ include/cectypes.h
@@ -309,6 +309,16 @@ namespace CEC {
 #define CEC_EXYNOS_VIRTUAL_COM		"Exynos"
 
 /*!
+ * the path to use for the i.MX CEC wire
+ */
+#define CEC_IMX_PATH		"/dev/mxc_hdmi_cec"
+
+/*!
+ * the name of the virtual COM port to use for the i.MX CEC wire
+ */
+#define CEC_IMX_VIRTUAL_COM		"i.MX"
+
+/*!
  * Mimimum client version
  */
 #define CEC_MIN_LIB_VERSION          3
@@ -877,7 +887,8 @@ typedef enum cec_adapter_type
   ADAPTERTYPE_P8_DAUGHTERBOARD = 0x2,
   ADAPTERTYPE_RPI              = 0x100,
   ADAPTERTYPE_TDA995x          = 0x200,
-  ADAPTERTYPE_EXYNOS           = 0x300
+  ADAPTERTYPE_EXYNOS           = 0x300,
+  ADAPTERTYPE_IMX	       = 0x400,
 } cec_adapter_type;
 
 /** force exporting through swig */
